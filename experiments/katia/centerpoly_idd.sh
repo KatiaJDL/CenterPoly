2196174eeb48c1e5136344fdaedbcaf57f7e2215
KatiaJDL <zirconium2015@gmail.com>
2022-05-17 17:04:02 -0400

    scripts

--- experiments/katia/centerpoly_idd.sh
+++ experiments/katia/centerpoly_idd.sh
@@ -3,5 +3,5 @@
 conda activate CenterPoly
 cd src
 
-#python main.py polydet --val_intervals 1 --exp_id from_smallhourglass --poly_weight 1 --depth_weight 0.1 --elliptical_gt --nbr_points 16 --dataset IDD --arch smallhourglass  --batch_size 4 --master_batch 4 --lr 2e-4 --load_model ../models/Small_Hourglass.pth
-#python test.py polydet --nms --exp_id from_cityscapes --nbr_points 16 --dataset IDD --arch smallhourglass  --load_model ../exp/IDD/polydet/from_cityscapes/model_best.pth
+#python main.py polydet --val_intervals 1 --exp_id from_smallhourglass --poly_weight 1 --depth_weight 0.1 --elliptical_gt --nbr_points 16 --dataset IDD --arch smallhourglass  --batch_size 4 --master_batch 4 --lr 2e-4 --load_model ../exp/cityscapes/polydet/from_ctdet_smhg_1cnv_16_pw1_B/model_best.pth
+#python test.py polydet --nms --exp_id from_cityscapes_TEST --nbr_points 16 --dataset IDD --arch smallhourglass  --load_model ../exp/IDD/polydet/from_cityscapes/model_best.pth
--- experiments/katia/centerpoly_iou_cityscapes.sh
+++ experiments/katia/centerpoly_iou_cityscapes.sh
@@ -5,9 +5,13 @@ conda activate CenterPoly
 # training from scratch
 python main.py polydet --val_intervals 24 --exp_id iou_loss --elliptical_gt --poly_weight 1 --nbr_points 16 --dataset cityscapes --arch smallhourglass  --batch_size 4 --master_batch 4 --lr 2e-4 --load_model ../models/ctdet_coco_hg.pth --poly_loss iou --num_epochs 60
 
-python test.py polydet --exp_id iou_loss_TEST --nbr_points 16 --dataset cityscapes --arch smallhourglass --load_model ../exp/cityscapes/polydet/ou_loss/model_best.pth
+python test.py polydet --exp_id iou_loss_TEST --nbr_points 16 --dataset cityscapes --arch smallhourglass --load_model ../exp/cityscapes/polydet/iou_loss/model_best.pth
 
 #with pretrained CenterPoly
 python main.py polydet --val_intervals 24 --exp_id pretrained_iou_loss --elliptical_gt --poly_weight 1 --nbr_points 16 --dataset cityscapes --arch smallhourglass  --batch_size 4 --master_batch 4 --lr 2e-4 --load_model ../exp/cityscapes/polydet/from_ctdet_smhg_1cnv_16_pw1_B/model_best.pth --poly_loss iou --num_epochs 240
 
 python test.py polydet --exp_id pretrained_iou_loss_TEST --nbr_points 16 --dataset cityscapes --arch smallhourglass --load_model ../exp/cityscapes/polydet/pretrained_iou_loss/model_best.pth
+
+# training from scratch with l1 + iou
+python main.py polydet --val_intervals 24 --exp_id iou_l1_loss --elliptical_gt --poly_weight 1 --nbr_points 16 --dataset cityscapes --arch smallhourglass  --batch_size 4 --master_batch 4 --lr 2e-4 --load_model ../models/ctdet_coco_hg.pth --poly_loss l1+iou
+
