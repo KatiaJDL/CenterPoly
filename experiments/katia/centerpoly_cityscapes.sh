efc66236f15250ea2cc3787b452e25a9b6ece0fc
KatiaJDL <k.jodogne-dellitto@orange.fr>
2022-12-06 12:24:05 -0500

    patch demo

--- experiments/katia/centerpoly_cityscapes.sh
+++ experiments/katia/centerpoly_cityscapes.sh
@@ -3,3 +3,5 @@ cd src
 
 # python main.py polydet --val_intervals 24 --exp_id from_ctdet_smhg_1cnv_16_pw1_B --elliptical_gt --poly_weight 1 --nbr_points 16 --dataset cityscapes --arch smallhourglass  --batch_size 4 --master_batch 4 --lr 2e-4 --load_model ../models/ctdet_coco_hg.pth
 python test.py polydet --exp_id from_ctdet_smhg_1cnv_16_pw1_B_TEST --nbr_points 16 --dataset cityscapes --arch smallhourglass --load_model ../exp/cityscapes/polydet/from_ctdet_smhg_1cnv_16_pw1_B/model_best.pth
+
+python test.py polydet --exp_id from_ctdet_smhg_1cnv_16_pw1_B_TEST_thresh010 --nbr_points 16 --dataset cityscapes --arch smallhourglass --load_model ../exp/cityscapes/polydet/from_ctdet_smhg_1cnv_16_pw1_B/model_best.pth --tresh 0.1
